9b2d1f53835fbaf3d16243e8a7adbe9baabccb15
solidDoWant <fred.heinecke@yahoo.com>
2026-01-06 00:36:36 -0600

    Added JEI info plugin
    Signed-off-by: solidDoWant <fred.heinecke@yahoo.com>

--- src/main/java/com/soliddowant/enderioconduitreplacer/integration/jei/JeiPlugin.java
+++ src/main/java/com/soliddowant/enderioconduitreplacer/integration/jei/JeiPlugin.java
@@ -26,26 +26,17 @@ public class JeiPlugin implements IModPlugin {
         String[] description = new String[] {
             TextFormatting.WHITE.toString() + TextFormatting.BOLD + "Conduit Replacer" + TextFormatting.RESET,
             "",
-            "A tool for quickly replacing conduits in a network.",
+            "A tool for replacing all conduits of a given type.",
             "",
-            TextFormatting.GOLD + "Basic Usage:" + TextFormatting.RESET,
-            "1. " + TextFormatting.AQUA + "Middle-click" + TextFormatting.RESET + " a conduit to set it as the " + TextFormatting.GREEN + "source",
-            "2. " + TextFormatting.AQUA + "Shift + Middle-click" + TextFormatting.RESET + " a conduit to set it as the " + TextFormatting.GREEN + "replacement",
-            "3. " + TextFormatting.AQUA + "Right-click" + TextFormatting.RESET + " to open the GUI and configure",
-            "4. " + TextFormatting.AQUA + "Left-click" + TextFormatting.RESET + " conduit bundles to replace matching conduits",
+            "Basic Usage:",
+            "1. " + TextFormatting.AQUA + "Right-click" + TextFormatting.RESET + " to open the GUI and configure, or " + TextFormatting.AQUA + "(Shift) Middle-click" + TextFormatting.RESET + " a conduit to set source/replacement",
+            "2. " + TextFormatting.AQUA + "Right-click" + TextFormatting.RESET + " conduit bundles to replace matching conduits",
             "",
-            TextFormatting.GOLD + "GUI:" + TextFormatting.RESET,
-            "• Shows the source and replacement conduit types",
-            "• Displays category compatibility (energy, item, fluid, etc.)",
-            "• Items can be dragged from JEI into the ghost slots",
+            "Items can be dragged from JEI into the GUI slots.",
             "",
-            TextFormatting.GOLD + "Requirements:" + TextFormatting.RESET,
+            "Requirements:",
             "• Source and replacement must be the same category",
-            "• Replacement conduits consumed from inventory (except creative mode)",
-            "• Double-click to confirm if upgrades/filters will be lost",
-            "",
-            TextFormatting.GRAY + "The replaced conduits will maintain their connections",
-            TextFormatting.GRAY + "but may lose some upgrades or filter settings."
+            "• Replacement conduits must be in your inventory",
         };
 
         registry.addIngredientInfo(conduitReplacer, VanillaTypes.ITEM, description);
